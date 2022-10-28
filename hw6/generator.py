1a8c72ed84a6a3fc3699f1a60c47958c19d8d5bc
DianaRez <reznikova.da@phystech.edu>
2022-10-28 17:21:12 +0300

    1

--- hw6/generator.py
+++ hw6/generator.py
@@ -1,16 +1,18 @@
 def generator_fib(finish = None):
     k = 0
     point1 = 0
+    yield point1
     point2 = 1
+    yield point2
     while True:
         res = point1 + point2
         yield res
         point1 = point2
         point2 = res
         k += 1
-        if k > finish - 1:
+        if k > finish - 3:
             return
 
-for i in generator_fib(5):
+for i in generator_fib(10):
     print(i)
 
